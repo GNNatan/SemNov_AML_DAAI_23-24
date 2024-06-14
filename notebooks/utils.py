5dfde39d14a8a17050305ea760eb7705eee5cff3
Guido Pio Natalizio <100683377+Guido-Pio-Natalizio@users.noreply.github.com>
2024-06-14 12:48:06 +0200

    Update utils.py

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -31,9 +31,9 @@ def render_cloud(cloud:np.ndarray,*, colors:np.array=None, cmap:str='RdYlGn', ti
     fig = plt.figure(figsize=(8, 8))
     ax = fig.add_subplot(111, projection='3d')
 
-    if colors is not None:
+    try:
         ax.scatter(x, -y, z, c=colors, cmap=cmap)
-    else:
+    except NameError:
         ax.scatter(x, -y, z)
     ax.axis('off')
     if title:
